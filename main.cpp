a17e85a5542edb09ffe8f0a7fdcb6961a1ab5de3
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-15 22:20:14 +0800

    add gl version information

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 // Richard S. Wright Jr.
 // rwright@starstonesoftware.com
 
+#include <cstdio>
 #include "gltools.h"	// OpenGL toolkit
 
 ///////////////////////////////////////////////////////////
@@ -32,7 +33,19 @@ int main(int argc, char* argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
     glutCreateWindow("Simple");
+
     glutDisplayFunc(RenderScene);
+
+    // 获取OpenGL版本号和厂商信息
+    const GLubyte *name = glGetString(GL_VENDOR);
+    const GLubyte *label = glGetString(GL_RENDERER);
+    const GLubyte *glVersion = glGetString(GL_VERSION);
+    const GLubyte *gluVersion = glGetString(GLU_VERSION);
+
+    printf("OpenGL实现厂商的名字：%s\n",name);
+    printf("渲染器标识符：%s\n",label);
+    printf("OpenGL实现的版本号：%s\n",glVersion);
+    printf("GLU工具库版本：%s\n",gluVersion);
 
     SetupRC();
 
