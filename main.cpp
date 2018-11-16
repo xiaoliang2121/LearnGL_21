0c8598de4c4bac5796379115e0c076d0dbb0744a
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-16 16:35:10 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -14,32 +14,105 @@
 static GLfloat xRot = 0.0f;
 static GLfloat yRot = 0.0f;
 
+// Flags for effects
+int iCull = 0;
+int iOutline = 0;
+int iDepth = 0;
+
 ///////////////////////////////////////////////////////////
+// Reset flags as appropriate in response to menu selections
+/**
+ * @brief ProcessMenu
+ * @param value
+ */
+void ProcessMenu(int value){
+    switch(value)
+    {
+    case 1:
+        iDepth = !iDepth;
+        break;
+    case 2:
+        iCull = !iCull;
+        break;
+    case 3:
+        iOutline = !iOutline;
+    default:
+        break;
+    }
+}
+
+
 // Called to draw scene
 void RenderScene(void)
     {
-    GLfloat y;                  // Storeage for varying Y coordinate
-    GLint factor = 3;           // Stippling factor
-    GLushort pattern = 0x5555;  // Stipple pattern
+    GLfloat x,y,angle;                  // Storeage for varying X Y coordinate
+    int iPivot = 1;                     // Used to flag alternating colors
 
     // Clear the window with current clearing color
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Turn culling on if flag is set
+    if(iCull)
+        glEnable(GL_CULL_FACE);
+    else
+        glDisable(GL_CULL_FACE);
+
+    // Enable depth testing if flag is set
+    if(iDepth)
+        glEnable(GL_DEPTH_TEST);
+    else
+        glDisable(GL_DEPTH_TEST);
+
+    // Draw back side as a polygon only, if flag is set
+    if(iOutline)
+        glPolygonMode(GL_BACK,GL_LINE);
+    else
+        glPolygonMode(GL_BACK,GL_FILL);
 
     glPushMatrix();
     glRotatef(xRot,1.0f,0.0f,0.0f);
     glRotatef(yRot,0.0f,1.0f,0.0f);
 
-    for(y=-90.0f; y<90.0f; y+=20.0f)
-    {
-        glLineStipple(factor,pattern);
+    // Begin a triangle fan
+    glBegin(GL_TRIANGLE_FAN);
+        glVertex3f(0.0f, 0.0f, 75.0f);
+        for(angle=0.0f; angle<(2.0f*GL_PI); angle+=(GL_PI/8.0f))
+        {
+            x = 50.0f*sin(angle);
+            y = 50.0f*cos(angle);
 
-        glBegin(GL_LINES);
-            glVertex2f(-80.0f,y);
-            glVertex2f(80.0f,y);
-        glEnd();
+            if(iPivot%2 == 0)
+                glColor3f(0.0f,1.0f,0.0f);
+            else
+                glColor3f(1.0f,0.0f,0.0f);
 
-        factor++;
-    }
+            iPivot++;
+
+            glVertex2f(x,y);
+        }
+    // Done drawing fan for cone
+    glEnd();
+
+    // Begin a new triangle fan to cover the bottom
+    glBegin(GL_TRIANGLE_FAN);
+        // Center of fan is at the origin
+        glVertex2f(0.0f,0.0f);
+        for(angle=0.0f; angle<(2.0f*GL_PI); angle+=(GL_PI/8.0f))
+        {
+            x = 50.0f*sin(angle);
+            y = 50.0f*cos(angle);
+
+            if(iPivot%2 == 0)
+                glColor3f(0.0f,1.0f,0.0f);
+            else
+                glColor3f(1.0f,0.0f,0.0f);
+
+            iPivot++;
+
+            glVertex2f(x,y);
+        }
+    // Done drawing the fan that covers the bottom
+    glEnd();
 
     glPopMatrix();
 
@@ -107,7 +180,8 @@ void SetupRC(void)
 
     glColor3f(0.0f,1.0f,0.0f);
 
-    glEnable(GL_LINE_STIPPLE);
+    glShadeModel(GL_FLAT);
+    glFrontFace(GL_CW);
     }
 
 ///////////////////////////////////////////////////////////
@@ -117,7 +191,14 @@ int main(int argc, char* argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800,600);
-    glutCreateWindow("Line Strips Example");
+    glutCreateWindow("Triangle Culling Example");
+
+    // Create the Menu
+    glutCreateMenu(ProcessMenu);
+    glutAddMenuEntry("Toggle depth test",1);
+    glutAddMenuEntry("Toggle cull backface",2);
+    glutAddMenuEntry("Toggle outline back",3);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 
     glutDisplayFunc(RenderScene);
     glutReshapeFunc(ChangeSize);
