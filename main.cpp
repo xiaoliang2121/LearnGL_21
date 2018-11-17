f0b22d5ed4a8d2aea19314d8d90c7f069ae90b01
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-17 14:32:05 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -11,88 +11,68 @@
 static GLfloat xRot = 0.0f;
 static GLfloat yRot = 0.0f;
 
-
-// Called to draw scene
-void RenderScene(void)
+// Change viewing volume and viewport.  Called when window is resized
+void ChangeSize(int w, int h)
     {
-    // Angle of revolution around the nucleus
-    static GLfloat fElect1 = 0.0f;
+    GLfloat nRange = 120.0f;
+    // Prevent a divide by zero
+    if(h == 0)
+        h = 1;
 
-    // Clear the window with current clearing color
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // Set Viewport to window dimensions
+    glViewport(0, 0, w, h);
 
-    // Reset the modelview matrix
-    glMatrixMode(GL_MODELVIEW);
+    // Reset coordinate system
+    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
 
-    // Translate the whole scene out and into view
-    // This is the initial viewing transformation
-    glTranslatef(0.0f, 0.0f, -100.0f);
+    // Establish clipping volume (left, right, bottom, top, near, far)
+    if (w <= h)
+        glOrtho (-nRange, nRange, -nRange*h/w, nRange*h/w, -nRange*2.0f, nRange*2.0f);
+    else
+        glOrtho (-nRange*w/h, nRange*w/h, -nRange, nRange, -nRange*2.0f, nRange*2.0f);
 
-    // Red Nucleus
-    glColor3ub(255, 0, 0);
-    glutSolidSphere(10.0f, 15, 15);
-
-    // Yellow Electrons
-    glColor3ub(255,255,0);
-
-    // First Electron Orbit
-    // Save viewing transformation
-    glPushMatrix();
-
-    // Rotate by angle of revolution
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-
-    // Translate out from origin to orbit distance
-    glTranslatef(90.0f, 0.0f, 0.0f);
-
-    // Draw the electron
-    glutSolidSphere(6.0f, 15, 15);
-
-
-    // Restore the viewing transformation
-    glPopMatrix();
-
-    // Second Electron Orbit
-    glPushMatrix();
-    glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(-70.0f, 0.0f, 0.0f);
-    glutSolidSphere(6.0f, 15, 15);
-    glPopMatrix();
-
-
-    // Third Electron Orbit
-    glPushMatrix();
-    glRotatef(360.0f-45.0f,0.0f, 0.0f, 1.0f);
-    glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
-    glTranslatef(0.0f, 0.0f, 60.0f);
-    glutSolidSphere(6.0f, 15, 15);
-    glPopMatrix();
-
-
-    // Increment the angle of revolution
-    fElect1 += 10.0f;
-    if(fElect1 > 360.0f)
-        fElect1 = 0.0f;
-
-    // Show the image
-    glutSwapBuffers();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     }
 
 
+
 // This function does any needed initialization on the rendering
-// context.
+// context.  Here it sets up and initializes the lighting for
+// the scene.
 void SetupRC()
     {
+    // Light values and coordinates
+    GLfloat  whiteLight[] = { 0.45f, 0.45f, 0.45f, 1.0f };
+    GLfloat  sourceLight[] = { 0.25f, 0.25f, 0.25f, 1.0f };
+    GLfloat	 lightPos[] = { -50.f, 25.0f, 250.0f, 0.0f };
+
     glEnable(GL_DEPTH_TEST);	// Hidden surface removal
     glFrontFace(GL_CCW);		// Counter clock-wise polygons face out
     glEnable(GL_CULL_FACE);		// Do not calculate inside of jet
 
-    // Black background
+    // Enable lighting
+    glEnable(GL_LIGHTING);
+
+    // Setup and enable light 0
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT,whiteLight);
+    glLightfv(GL_LIGHT0,GL_AMBIENT,sourceLight);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,sourceLight);
+    glLightfv(GL_LIGHT0,GL_POSITION,lightPos);
+    glEnable(GL_LIGHT0);
+
+    // Enable color tracking
+    glEnable(GL_COLOR_MATERIAL);
+
+    // Set Material properties to follow glColor values
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+    // Black blue background
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
     }
 
+// Respond to arrow keys
 void SpecialKeys(int key, int x, int y)
     {
     if(key == GLUT_KEY_UP)
@@ -107,55 +87,163 @@ void SpecialKeys(int key, int x, int y)
     if(key == GLUT_KEY_RIGHT)
         yRot += 5.0f;
 
-    if(key > 356.0f)
-        xRot = 0.0f;
-
-    if(key < -1.0f)
-        xRot = 355.0f;
-
-    if(key > 356.0f)
-        yRot = 0.0f;
-
-    if(key < -1.0f)
-        yRot = 355.0f;
+        xRot = (GLfloat)((const int)xRot % 360);
+        yRot = (GLfloat)((const int)yRot % 360);
 
     // Refresh the Window
     glutPostRedisplay();
     }
 
-void TimerFunc(int value)
+
+// Called to draw scene
+void RenderScene(void)
     {
-    glutPostRedisplay();
-    glutTimerFunc(100, TimerFunc, 1);
+    float fZ,bZ;
+
+    // Clear the window with current clearing color
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    fZ = 100.0f;
+    bZ = -100.0f;
+
+    // Save the matrix state and do the rotations
+    glPushMatrix();
+    glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+    glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+
+    // Set material color, Red
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    // Front Face ///////////////////////////////////
+    glBegin(GL_QUADS);
+        // Pointing straight out Z
+        glNormal3f(0.0f, 0.0f, 1.0f);
+
+        // Left Panel
+        glVertex3f(-50.0f, 50.0f, fZ);
+        glVertex3f(-50.0f, -50.0f, fZ);
+        glVertex3f(-35.0f, -50.0f, fZ);
+        glVertex3f(-35.0f,50.0f,fZ);
+
+        // Right Panel
+        glVertex3f(50.0f, 50.0f, fZ);
+        glVertex3f(35.0f, 50.0f, fZ);
+        glVertex3f(35.0f, -50.0f, fZ);
+        glVertex3f(50.0f,-50.0f,fZ);
+
+        // Top Panel
+        glVertex3f(-35.0f, 50.0f, fZ);
+        glVertex3f(-35.0f, 35.0f, fZ);
+        glVertex3f(35.0f, 35.0f, fZ);
+        glVertex3f(35.0f, 50.0f,fZ);
+
+        // Bottom Panel
+        glVertex3f(-35.0f, -35.0f, fZ);
+        glVertex3f(-35.0f, -50.0f, fZ);
+        glVertex3f(35.0f, -50.0f, fZ);
+        glVertex3f(35.0f, -35.0f,fZ);
+
+        // Top length section ////////////////////////////
+        // Normal points up Y axis
+        glNormal3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(-50.0f, 50.0f, fZ);
+        glVertex3f(50.0f, 50.0f, fZ);
+        glVertex3f(50.0f, 50.0f, bZ);
+        glVertex3f(-50.0f,50.0f,bZ);
+
+        // Bottom section
+        glNormal3f(0.0f, -1.0f, 0.0f);
+        glVertex3f(-50.0f, -50.0f, fZ);
+        glVertex3f(-50.0f, -50.0f, bZ);
+        glVertex3f(50.0f, -50.0f, bZ);
+        glVertex3f(50.0f, -50.0f, fZ);
+
+        // Left section
+        glNormal3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(50.0f, 50.0f, fZ);
+        glVertex3f(50.0f, -50.0f, fZ);
+        glVertex3f(50.0f, -50.0f, bZ);
+        glVertex3f(50.0f, 50.0f, bZ);
+
+        // Right Section
+        glNormal3f(-1.0f, 0.0f, 0.0f);
+        glVertex3f(-50.0f, 50.0f, fZ);
+        glVertex3f(-50.0f, 50.0f, bZ);
+        glVertex3f(-50.0f, -50.0f, bZ);
+        glVertex3f(-50.0f, -50.0f, fZ);
+    glEnd();
+
+    glFrontFace(GL_CW);		// clock-wise polygons face out
+
+    glBegin(GL_QUADS);
+        // Back section
+        // Pointing straight out Z
+        glNormal3f(0.0f, 0.0f, -1.0f);
+
+        // Left Panel
+        glVertex3f(-50.0f, 50.0f, bZ);
+        glVertex3f(-50.0f, -50.0f, bZ);
+        glVertex3f(-35.0f, -50.0f, bZ);
+        glVertex3f(-35.0f,50.0f,bZ);
+
+        // Right Panel
+        glVertex3f(50.0f, 50.0f, bZ);
+        glVertex3f(35.0f, 50.0f, bZ);
+        glVertex3f(35.0f, -50.0f, bZ);
+        glVertex3f(50.0f,-50.0f,bZ);
+
+        // Top Panel
+        glVertex3f(-35.0f, 50.0f, bZ);
+        glVertex3f(-35.0f, 35.0f, bZ);
+        glVertex3f(35.0f, 35.0f, bZ);
+        glVertex3f(35.0f, 50.0f,bZ);
+
+        // Bottom Panel
+        glVertex3f(-35.0f, -35.0f, bZ);
+        glVertex3f(-35.0f, -50.0f, bZ);
+        glVertex3f(35.0f, -50.0f, bZ);
+        glVertex3f(35.0f, -35.0f,bZ);
+
+        // Insides /////////////////////////////
+        glColor3f(0.75f, 0.75f, 0.75f);
+
+        // Normal points up Y axis
+        glNormal3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(-35.0f, 35.0f, fZ);
+        glVertex3f(35.0f, 35.0f, fZ);
+        glVertex3f(35.0f, 35.0f, bZ);
+        glVertex3f(-35.0f,35.0f,bZ);
+
+        // Bottom section
+        glNormal3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(-35.0f, -35.0f, fZ);
+        glVertex3f(-35.0f, -35.0f, bZ);
+        glVertex3f(35.0f, -35.0f, bZ);
+        glVertex3f(35.0f, -35.0f, fZ);
+
+        // Left section
+        glNormal3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-35.0f, 35.0f, fZ);
+        glVertex3f(-35.0f, 35.0f, bZ);
+        glVertex3f(-35.0f, -35.0f, bZ);
+        glVertex3f(-35.0f, -35.0f, fZ);
+
+        // Right Section
+        glNormal3f(-1.0f, 0.0f, 0.0f);
+        glVertex3f(35.0f, 35.0f, fZ);
+        glVertex3f(35.0f, -35.0f, fZ);
+        glVertex3f(35.0f, -35.0f, bZ);
+        glVertex3f(35.0f, 35.0f, bZ);
+    glEnd();
+
+    glFrontFace(GL_CCW);		// Counter clock-wise polygons face out
+
+    // Restore the matrix state
+    glPopMatrix();
+
+    // Buffer swap
+    glutSwapBuffers();
     }
-
-
-void ChangeSize(int w, int h)
-    {
-    GLfloat nRange = 100.0f;
-
-    // Prevent a divide by zero
-    if(h == 0)
-        h = 1;
-
-    // Set Viewport to window dimensions
-    glViewport(0, 0, w, h);
-
-
-    // Reset coordinate system
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-
-    // Establish clipping volume (left, right, bottom, top, near, far)
-    if (w <= h)
-        glOrtho (-nRange, nRange, nRange*h/w, -nRange*h/w, -nRange*2.0f, nRange*2.0f);
-    else
-        glOrtho (-nRange*w/h, nRange*w/h, nRange, -nRange, -nRange*2.0f, nRange*2.0f);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    }
-
 ///////////////////////////////////////////////////////////
 // Main program entry point
 int main(int argc, char* argv[])
@@ -163,13 +251,11 @@ int main(int argc, char* argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800,600);
-    glutCreateWindow("OpenGL Atom");
+    glutCreateWindow("Orthographic Projection");
 
     glutReshapeFunc(ChangeSize);
     glutSpecialFunc(SpecialKeys);
     glutDisplayFunc(RenderScene);
-
-    glutTimerFunc(500, TimerFunc, 1);
 
     // 获取OpenGL版本号和厂商信息
     const GLubyte *name = glGetString(GL_VENDOR);
