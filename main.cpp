ee037e305b681a9d6326ae0d4223a2cc5c8a3cda
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-17 21:07:51 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 #include <cstdio>
 #include "gltools.h"	// OpenGL toolkit
+#include "math3d.h"
 
 // Rotation amounts
 static GLfloat xRot = 0.0f;
@@ -14,133 +15,236 @@ static GLfloat yRot = 0.0f;
 
 // Called to draw scene
 void RenderScene(void)
-    {
+{
+    M3DVector3f vNormal;	// Storeage for calculated surface normal
+
     // Clear the window with current clearing color
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Save matrix state and do the rotation
+    // Save the matrix state and do the rotations
     glPushMatrix();
     glRotatef(xRot, 1.0f, 0.0f, 0.0f);
     glRotatef(yRot, 0.0f, 1.0f, 0.0f);
 
 
-    // Nose Cone /////////////////////////////
-    // White
-    glColor3ub(255, 255, 255);
+    // Nose Cone - Points straight down
+    // Set material color
+    glColor3ub(128, 128, 128);
     glBegin(GL_TRIANGLES);
+        glNormal3f(0.0f, -1.0f, 0.0f);
         glVertex3f(0.0f, 0.0f, 60.0f);
         glVertex3f(-15.0f, 0.0f, 30.0f);
         glVertex3f(15.0f,0.0f,30.0f);
 
-        // Black
-        glColor3ub(0,0,0);
-        glVertex3f(15.0f,0.0f,30.0f);
-        glVertex3f(0.0f, 15.0f, 30.0f);
-        glVertex3f(0.0f, 0.0f, 60.0f);
 
-        // Red
-        glColor3ub(255,0,0);
-        glVertex3f(0.0f, 0.0f, 60.0f);
-        glVertex3f(0.0f, 15.0f, 30.0f);
-        glVertex3f(-15.0f,0.0f,30.0f);
+        // Verticies for this panel
+        {
+        M3DVector3f vPoints[3] = {{ 15.0f, 0.0f,  30.0f},
+                                        { 0.0f,  15.0f, 30.0f},
+                                        { 0.0f,  0.0f,  60.0f}};
+
+        // Calculate the normal for the plane
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
 
 
-    // Body of the Plane ////////////////////////
-        // Green
-        glColor3ub(0,255,0);
-        glVertex3f(-15.0f,0.0f,30.0f);
-        glVertex3f(0.0f, 15.0f, 30.0f);
-        glVertex3f(0.0f, 0.0f, -56.0f);
+        {
+        M3DVector3f vPoints[3] = {{ 0.0f, 0.0f, 60.0f },
+                                  { 0.0f, 15.0f, 30.0f },
+                                  { -15.0f, 0.0f, 30.0f }};
 
-        glColor3ub(255,255,0);
-        glVertex3f(0.0f, 0.0f, -56.0f);
-        glVertex3f(0.0f, 15.0f, 30.0f);
-        glVertex3f(15.0f,0.0f,30.0f);
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
 
-        glColor3ub(0, 255, 255);
+
+        // Body of the Plane ////////////////////////
+        {
+        M3DVector3f vPoints[3] = {{ -15.0f, 0.0f, 30.0f },
+                                  { 0.0f, 15.0f, 30.0f },
+                                  { 0.0f, 0.0f, -56.0f }};
+
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+        {
+        M3DVector3f vPoints[3] = {{ 0.0f, 0.0f, -56.0f },
+                                  { 0.0f, 15.0f, 30.0f },
+                                  { 15.0f,0.0f,30.0f }};
+
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+
+        glNormal3f(0.0f, -1.0f, 0.0f);
         glVertex3f(15.0f,0.0f,30.0f);
         glVertex3f(-15.0f, 0.0f, 30.0f);
         glVertex3f(0.0f, 0.0f, -56.0f);
 
-    ///////////////////////////////////////////////
-    // Left wing
-    // Large triangle for bottom of wing
-        glColor3ub(128,128,128);
-        glVertex3f(0.0f,2.0f,27.0f);
-        glVertex3f(-60.0f, 2.0f, -8.0f);
-        glVertex3f(60.0f, 2.0f, -8.0f);
+        ///////////////////////////////////////////////
+        // Left wing
+        // Large triangle for bottom of wing
+        {
+        M3DVector3f vPoints[3] = {{ 0.0f,2.0f,27.0f },
+                                  { -60.0f, 2.0f, -8.0f },
+                                  { 60.0f, 2.0f, -8.0f }};
 
-        glColor3ub(64,64,64);
-        glVertex3f(60.0f, 2.0f, -8.0f);
-        glVertex3f(0.0f, 7.0f, -8.0f);
-        glVertex3f(0.0f,2.0f,27.0f);
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
 
-        glColor3ub(192,192,192);
-        glVertex3f(60.0f, 2.0f, -8.0f);
-        glVertex3f(-60.0f, 2.0f, -8.0f);
-        glVertex3f(0.0f,7.0f,-8.0f);
 
-    // Other wing top section
-        glColor3ub(64,64,64);
-        glVertex3f(0.0f,2.0f,27.0f);
-        glVertex3f(0.0f, 7.0f, -8.0f);
-        glVertex3f(-60.0f, 2.0f, -8.0f);
+        {
+        M3DVector3f vPoints[3] = {{ 60.0f, 2.0f, -8.0f},
+                                   {0.0f, 7.0f, -8.0f},
+                                   {0.0f,2.0f,27.0f }};
 
-    // Tail section///////////////////////////////
-    // Bottom of back fin
-        glColor3ub(255,128,255);
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+        {
+        M3DVector3f vPoints[3] = {{60.0f, 2.0f, -8.0f},
+                    {-60.0f, 2.0f, -8.0f},
+                    {0.0f,7.0f,-8.0f }};
+
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+        {
+        M3DVector3f vPoints[3] = {{0.0f,2.0f,27.0f},
+                                    {0.0f, 7.0f, -8.0f},
+                                    {-60.0f, 2.0f, -8.0f}};
+
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+
+        // Tail section///////////////////////////////
+        // Bottom of back fin
+        glNormal3f(0.0f, -1.0f, 0.0f);
         glVertex3f(-30.0f, -0.50f, -57.0f);
         glVertex3f(30.0f, -0.50f, -57.0f);
         glVertex3f(0.0f,-0.50f,-40.0f);
 
-        // top of left side
-        glColor3ub(255,128,0);
-        glVertex3f(0.0f,-0.5f,-40.0f);
-        glVertex3f(30.0f, -0.5f, -57.0f);
-        glVertex3f(0.0f, 4.0f, -57.0f);
+        {
+        M3DVector3f vPoints[3] = {{ 0.0f,-0.5f,-40.0f },
+                                  {30.0f, -0.5f, -57.0f},
+                                {0.0f, 4.0f, -57.0f }};
 
-        // top of right side
-        glColor3ub(255,128,0);
-        glVertex3f(0.0f, 4.0f, -57.0f);
-        glVertex3f(-30.0f, -0.5f, -57.0f);
-        glVertex3f(0.0f,-0.5f,-40.0f);
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
 
-        // back of bottom of tail
-        glColor3ub(255,255,255);
-        glVertex3f(30.0f,-0.5f,-57.0f);
-        glVertex3f(-30.0f, -0.5f, -57.0f);
-        glVertex3f(0.0f, 4.0f, -57.0f);
 
-        // Top of Tail section left
-        glColor3ub(255,0,0);
-        glVertex3f(0.0f,0.5f,-40.0f);
-        glVertex3f(3.0f, 0.5f, -57.0f);
-        glVertex3f(0.0f, 25.0f, -65.0f);
+        {
+        M3DVector3f vPoints[3] = {{ 0.0f, 4.0f, -57.0f },
+                { -30.0f, -0.5f, -57.0f },
+                { 0.0f,-0.5f,-40.0f }};
 
-        glColor3ub(255,0,0);
-        glVertex3f(0.0f, 25.0f, -65.0f);
-        glVertex3f(-3.0f, 0.5f, -57.0f);
-        glVertex3f(0.0f,0.5f,-40.0f);
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
 
-        // Back of horizontal section
-        glColor3ub(128,128,128);
-        glVertex3f(3.0f,0.5f,-57.0f);
-        glVertex3f(-3.0f, 0.5f, -57.0f);
-        glVertex3f(0.0f, 25.0f, -65.0f);
+        {
+        M3DVector3f vPoints[3] = {{ 30.0f,-0.5f,-57.0f },
+                    { -30.0f, -0.5f, -57.0f },
+                    { 0.0f, 4.0f, -57.0f }};
 
-    glEnd(); // Of Jet
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
 
+        {
+        M3DVector3f vPoints[3] = {{ 0.0f,0.5f,-40.0f },
+                    { 3.0f, 0.5f, -57.0f },
+                    { 0.0f, 25.0f, -65.0f }};
+
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+
+        {
+        M3DVector3f vPoints[3] = {{ 0.0f, 25.0f, -65.0f },
+                    { -3.0f, 0.5f, -57.0f},
+                    { 0.0f,0.5f,-40.0f }};
+
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+        {
+        M3DVector3f vPoints[3] = {{ 3.0f,0.5f,-57.0f },
+                    { -3.0f, 0.5f, -57.0f },
+                    { 0.0f, 25.0f, -65.0f }};
+
+        m3dFindNormal(vNormal, vPoints[0], vPoints[1], vPoints[2]);
+        glNormal3fv(vNormal);
+        glVertex3fv(vPoints[0]);
+        glVertex3fv(vPoints[1]);
+        glVertex3fv(vPoints[2]);
+        }
+
+
+        glEnd();
+
+        // Restore the matrix state
     glPopMatrix();
-
     // Display the results
     glutSwapBuffers();
-    }
+}
 
 // This function does any needed initialization on the rendering
 // context.
 void SetupRC()
-    {
-    GLfloat ambientLight[] = {1.0f,1.0f,1.0f,1.0f};
+{
+    GLfloat ambientLight[] = {0.3f,0.3f,0.3f,1.0f};
+    GLfloat diffuseLight[] = {0.7f,0.7f,0.7f,1.0f};
 
     glEnable(GL_DEPTH_TEST);	// Hidden surface removal
     glEnable(GL_CULL_FACE);		// Do not calculate inside of jet
@@ -148,14 +252,19 @@ void SetupRC()
 
     // 打开设置背景光
     glEnable(GL_LIGHTING);
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT,ambientLight);
+//    glLightModelfv(GL_LIGHT_MODEL_AMBIENT,ambientLight);
+    glLightfv(GL_LIGHT0,GL_AMBIENT,ambientLight);
+    glLightfv(GL_LIGHT0,GL_DIFFUSE,diffuseLight);
+    glEnable(GL_LIGHT0);
 
     glEnable(GL_COLOR_MATERIAL);    // 启用材料颜色追踪
     glColorMaterial(GL_FRONT,GL_AMBIENT_AND_DIFFUSE);
 
     // Nice light blue
-    glClearColor(0.0f, 0.0f, 0.5f,1.0f);
-    }
+    glClearColor(0.0f, 0.0f, 1.0f,1.0f);
+
+    glEnable(GL_NORMALIZE);
+}
 
 void SpecialKeys(int key, int x, int y)
     {
