d319c98f76e8d4347bb4a665acd0904c59aff909
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-17 12:09:54 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -6,24 +6,68 @@
 
 #include <cstdio>
 #include "gltools.h"	// OpenGL toolkit
+#include <cmath>
 
 ///////////////////////////////////////////////////////////
 // Called to draw scene
 void RenderScene(void)
     {
-    // Clear the window with current clearing color
-    glClear(GL_COLOR_BUFFER_BIT);
+        static GLdouble dRadius = 0.1;
+        static GLdouble dAngle = 0.0;
 
+        // Clear blue window
+        glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
 
-    // Flush drawing commands
-    glFlush();
-    }
+        if(dAngle == 0.0)
+            glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_POINTS);
+            glVertex2d(dRadius * cos(dAngle), dRadius * sin(dAngle));
+        glEnd();
+
+        dRadius *= 1.01;
+        dAngle += 0.1;
+
+        if(dAngle > 30.0)
+            {
+            dRadius = 0.1;
+            dAngle = 0.0;
+            }
+
+        glFlush();
+        }
 
 ///////////////////////////////////////////////////////////
-// Setup the rendering state
-void SetupRC(void)
+// Trigger a repaint
+void Timer(int value)
     {
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glutTimerFunc(50,Timer, 0);
+    glutPostRedisplay();
+    }
+
+
+///////////////////////////////////////////////////////////
+// Set viewport and projection
+void ChangeSize(int w, int h)
+    {
+    // Prevent a divide by zero
+    if(h == 0)
+        h = 1;
+
+    // Set Viewport to window dimensions
+        glViewport(0, 0, w, h);
+
+
+    // Set the perspective coordinate system
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // Set 2D Coordinate system
+    gluOrtho2D(-4.0, 4.0, -3.0, 3.0);
+
+    // Modelview matrix reset
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     }
 
 ///////////////////////////////////////////////////////////
@@ -31,10 +75,12 @@ void SetupRC(void)
 int main(int argc, char* argv[])
     {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutCreateWindow("Simple");
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800,600);
+    glutCreateWindow("OpenGL Single Buffered");
 
     glutDisplayFunc(RenderScene);
+    glutReshapeFunc(ChangeSize);
 
     // 获取OpenGL版本号和厂商信息
     const GLubyte *name = glGetString(GL_VENDOR);
@@ -47,8 +93,8 @@ int main(int argc, char* argv[])
     printf("OpenGL实现的版本号：%s\n",glVersion);
     printf("GLU工具库版本：%s\n",gluVersion);
 
-    SetupRC();
-
+//    SetupRC();
+    glutTimerFunc(50,Timer,0);
     glutMainLoop();
 
     return 0;
