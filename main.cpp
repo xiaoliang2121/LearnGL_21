ac7fdfd751bbe56fbf2e5ac1662944c376fc499e
xiaoliang2121 <xiaoliang135xia@gmail.com>
2018-11-17 12:17:09 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -10,14 +10,47 @@
 ///////////////////////////////////////////////////////////
 // Called to draw scene
 void RenderScene(void)
-    {
+{
     // Clear the window with current clearing color
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    glScissor(100, 100, 600, 400);
+    glEnable(GL_SCISSOR_TEST);
+    glClear(GL_COLOR_BUFFER_BIT);
 
+    glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+    glScissor(200, 200, 400, 200);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDisable(GL_SCISSOR_TEST);
     // Flush drawing commands
-    glFlush();
-    }
+    glutSwapBuffers();
+}
+
+// Set viewport and projection
+void ChangeSize(int w, int h)
+{
+    // Prevent a divide by zero
+    if(h == 0)
+        h = 1;
+
+    // Set Viewport to window dimensions
+        glViewport(0, 0, w, h);
+
+
+    // Set the perspective coordinate system
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // Set 2D Coordinate system
+    gluOrtho2D(-4.0, 4.0, -3.0, 3.0);
+
+    // Modelview matrix reset
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
 
 ///////////////////////////////////////////////////////////
 // Setup the rendering state
@@ -31,10 +64,12 @@ void SetupRC(void)
 int main(int argc, char* argv[])
     {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutCreateWindow("Simple");
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800,600);
+    glutCreateWindow("OpenGL Scissor");
 
     glutDisplayFunc(RenderScene);
+    glutReshapeFunc(ChangeSize);
 
     // 获取OpenGL版本号和厂商信息
     const GLubyte *name = glGetString(GL_VENDOR);
